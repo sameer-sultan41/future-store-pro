e604b187743470bb5ae588821ae48b19de2f706a
Muhammad Sameer Sultan <158552837+sameer-sultan41@users.noreply.github.com>
2025-10-12 01:28:53 +0500

    revert

--- src/actions/product/deals.ts
+++ src/actions/product/deals.ts
@@ -2,14 +2,20 @@
 
 import { createSupabaseServer } from "@/shared/lib/supabaseClient";
 import { cookies } from "next/headers";
- const supabase = createSupabaseServer();
+
+
+function getCurrencyCodeFromCookie(): string {
+  const cookieStore = cookies();
+  const currency = cookieStore.get("currency");
+  return currency?.value || "PKR";
+}
 
 
 
 
 
 export const getTodayDeals = async (locale: string = "en") => {
- 
+  const supabase = createSupabaseServer();
   try {
     const { data, error } = await supabase
       .from("flash_deals")
@@ -40,16 +46,6 @@ export const getTodayDeals = async (locale: string = "en") => {
       .gt("end_date", new Date().toISOString())
       .eq("flash_deal_products.product.product_translations.language_code", locale);
     // Only the requested translation is returned
-
-    // get currency info
-    const { currencyData, currencyError } = await getCurrency();
-    if (currencyError) {
-      console.error("Error fetching currency:", currencyError);
-      return { data: null, error: "Error fetching currency" };
-    }
-    const exchangeRate = currencyData ? currencyData.exchange_rate_to_usd : 1; // Default to 1 if not found
-
-    // Convert prices based on exchange rate
     return { data: data as FlashDeal[] | null, error: error ? error.message ?? String(error) : null };
   } catch (error) {
     return { data: null, error: "Can't fetch today's deals" };
