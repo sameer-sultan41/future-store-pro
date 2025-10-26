f24c14535a0605bb6c22c0ed737b009ad1ed53df
Muhammad Sameer Sultan <158552837+sameer-sultan41@users.noreply.github.com>
2025-10-26 18:58:43 +0500

    fix detail page

--- src/app/[locale]/(store)/product/[productUrl]/page.tsx
+++ src/app/[locale]/(store)/product/[productUrl]/page.tsx
@@ -8,12 +8,12 @@ import { useEffect, useState } from "react";
 import { getOneProduct, getProductByUrl } from "@/actions/product/product";
 import { getShippingRates, ShippingRate, ShippingAddress, ShippingPackage } from "@/actions/shipping/shipping";
 import Gallery from "@/domains/product/components/gallery";
-import ProductBoard from "@/domains/product/components/productBoard";
 import ProductCard from "@/app/[locale]/(store)/(home)/_components/TopProductCard";
 import { TopProducts } from "@/domains/product/constants";
 import { LikeIcon, MinusIcon } from "@/shared/components/icons/svgIcons";
 import { SK_Box } from "@/shared/components/UI/skeleton";
 import { TProductPageInfo } from "@/shared/types/product";
+import ProductBoard from "./_components/ProductBoard";
 
 export interface ProductVariantType {
   name: string;
